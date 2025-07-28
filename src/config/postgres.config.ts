abf2ee2f1854e793d51779832d2e1203a7fb1807
Observersss <heavyobjectics@gmail.com>
2025-07-28 14:24:46 +0300

    setup postgres

---
+++ src/config/postgres.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('postgres', () => ({
+    port: Number(process.env.POSTGRES_PORT),
+    host: String(process.env.POSTGRES_HOST),
+    db: String(process.env.POSTGRES_DB),
+    user: String(process.env.POSTGRES_APP_USER),
+    pass: String(process.env.POSTGRES_APP_PASSWORD),
+}));
---
+++ src/modules/postgres/postgres.providers.ts
@@ -0,0 +1,35 @@
+import postgresConfig from '../../config/postgres.config';
+import appConfig from '../../config/app.config';
+import { ConfigType } from '@nestjs/config';
+import { POSTGRES, SEQUELIZE } from './constants';
+import { Sequelize } from 'sequelize-typescript';
+
+export const postgresProviders = [
+  {
+    provide: SEQUELIZE,
+    inject: [postgresConfig.KEY, appConfig.KEY],
+    useFactory: async (
+      postgresConf: ConfigType<typeof postgresConfig>,
+      appConf: ConfigType<typeof appConfig>,
+    ) => {
+
+      const sequelize = new Sequelize({
+        logging: console.log,
+        dialect: POSTGRES,
+        host: postgresConf.host,
+        port: postgresConf.port,
+        database: postgresConf.db,
+        username: postgresConf.user,
+        password: postgresConf.pass,
+      });
+
+      sequelize.addModels([]);
+
+      if (appConf.node_dev === 'testing') {
+        await sequelize.sync({ force: true });
+      }
+
+      return sequelize;
+    },
+  },
+];
